c43cb68e09bb0299ad5cb7a97a0e0cff29264b49
priyankav <priyankan.@mouritech.com>
2022-11-11 13:10:57 +0530

    random_password_gen.py

--- Json_to_csv.py
+++ Json_to_csv.py
@@ -1,42 +1,17 @@
 import json
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     try:
-        with open print([
-    {
-        "Name": "Priya",
-        "age" : 23,
-        "birth year": "1999"
-    }],
-        print([
-
-    {
-        "Name": "Raji",
-        "age" : 22,
-        "birth year":2000
-    },
-    ]),'r') as f:
+        with open('input.json','r') as f:
             data = json.loads(f.read())
 
             output = ','.join([*data[0]])
             for obj in data:
-                output += f'\n{obj["Name"]},{obj["age"]},{obj["birth year"]}'
+                output += f'\n{obj["Name"]},{obj["age"]},{obj["birthyear"]}'
 
-            with open('output.csv', 'w') as f:
+            with open('output.csv' ,'w') as f:
                 f.write(output)
     except Exception as ex:
         print(f'Error: {str(ex)}')
-        print([
-            {
-                "Name": "Priya",
-                "age": 23,
-                "birth year": "1999"
-            },
 
-            {
-                "Name": "Raji",
-                "age": 22,
-                "birth year": 2000
-            },
-        ])
 
